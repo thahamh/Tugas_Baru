bb6b80390e56ba7d1060ee668152e4e73502a247
thahamh <32389972+thahamh@users.noreply.github.com>
2018-01-01 22:04:56 -0800

    Menampilkan Nilai X

--- Menggunakan_for.cpp
+++ Menggunakan_for.cpp
@@ -6,5 +6,11 @@ int main(){
 	cout.setf(ios::fixed);
 	 cout.setf(ios::showpoint);
      cout.precision(4);
-     	
+     
+	 cout << "MATRIKS X : "<<endl;
+	double ac[3][5]={ {4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2} };
+	for(int i=0;i<3;i++)
+		for(int j=0;j<5;j++)
+		cout << ac[i][j] << " | ";
+		cout<<endl;	
 }
